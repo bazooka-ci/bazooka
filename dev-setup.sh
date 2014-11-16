09d58b0e1048f81ec76addf4049d3f4bf946a5d5
Eric Bellemon <eric.bellemon@gmail.com>
2014-11-16 22:07:22 +0100

    Fix wrong path on dev-setup

--- dev-setup.sh
+++ dev-setup.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-: ${GOPATH:?"GOPATH has to be set. See https://golang.org/doc/code.html#GOPATH for more information."}
-
-go_projects=( "parser" "parser/golang" "parser/java" "orchestration" "server" "cli" )
+go_projects=( "parser" "parserlang/golang" "parserlang/java" "orchestration" "server" "cli" )
 
 for project in "${go_projects[@]}"
 do
-  pushd "$GOPATH/src/github.com/haklop/bazooka/$project"
+  pushd "$project"
     go get -u -v ./...
   popd
 done
