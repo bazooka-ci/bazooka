65ccc369db0121b942b9145afb38fd1b7830be21
Julien Vey <vey.julien@gmail.com>
2014-11-19 12:43:54 +0100

    add links

--- orchestration/runner.go
+++ orchestration/runner.go
@@ -54,9 +54,12 @@ func runContainer(client *docker.Docker, buildImage BuiltImage, env map[string]s
 	}
 
 	serviceContainers := []*docker.Container{}
+	containerLinks := []string{}
 	for _, service := range servicesList {
+		name := fmt.Sprintf("service-%s-%s-%d", env[BazookaEnvProjectID], env[BazookaEnvJobID], buildImage.VariantID)
+		containerLinks = append(containerLinks, fmt.Sprintf("%s:%s", name, service))
 		serviceContainer, err := client.Run(&docker.RunOptions{
-			Name:   fmt.Sprintf("service-%s-%s-%d", env[BazookaEnvProjectID], env[BazookaEnvJobID], buildImage.VariantID),
+			Name:   name,
 			Image:  service,
 			Detach: true,
 		})
@@ -70,6 +73,7 @@ func runContainer(client *docker.Docker, buildImage BuiltImage, env map[string]s
 	// TODO link containers
 	container, err := client.Run(&docker.RunOptions{
 		Image:  buildImage.Image,
+		Links:  containerLinks,
 		Detach: true,
 	})
 	if err != nil {
