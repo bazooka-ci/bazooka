db43460350b2da3ff5dd4dac893186d972adfd09
Eric Bellemon <eric.bellemon@gmail.com>
2014-11-24 14:58:47 +0100

    Execute docker as root under linux

--- build-devimages.sh
+++ build-devimages.sh
@@ -8,11 +8,13 @@ if [ "$(uname)" != "Darwin" ]; then
   s=sudo
 fi
 
+export PREFIX=$s
+
 docker_projects=( "parser" "parserlang/golang" "parserlang/java" "orchestration" "server")
 
 for project in "${docker_projects[@]}"
 do
   pushd "$GOPATH/src/github.com/haklop/bazooka/$project"
-    $s make devimage
+    make devimage
   popd
 done
--- build-images.sh
+++ build-images.sh
@@ -8,12 +8,14 @@ if [ "$(uname)" != "Darwin" ]; then
   s=sudo
 fi
 
+export PREFIX=$s
+
 docker_projects=( "parser" "parserlang/golang" "parserlang/java" "orchestration" \
 "server" "runner/golang" "runner/java" "scm/git" )
 
 for project in "${docker_projects[@]}"
 do
   pushd "$GOPATH/src/github.com/haklop/bazooka/$project"
-    $s make
+    make
   popd
 done
