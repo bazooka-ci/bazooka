d29a7c16421c5e03d177dc6785d35b6cada06b7f
Eric Bellemon <eric.bellemon@gmail.com>
2014-12-03 23:41:01 +0100

    Create ByField method on mongo connector

--- commons/mongo/connector.go
+++ commons/mongo/connector.go
@@ -22,21 +22,23 @@ type MongoConnector struct {
 
 type NotFoundError struct {
 	Collection string
-	Id         string
+	Field      string
+	Value      string
 }
 
 func (n *NotFoundError) Error() string {
-	return fmt.Sprintf("%s[%s] not found", n.Collection, n.Id)
+	return fmt.Sprintf("%s[%s:%s] not found", n.Collection, n.Field, n.Value)
 }
 
 type ManyFoundError struct {
 	Collection string
-	Id         string
+	Field      string
+	Value      string
 	Count      int
 }
 
 func (m *ManyFoundError) Error() string {
-	return fmt.Sprintf("%s[%s] returned %d results", m.Collection, m.Id, m.Count)
+	return fmt.Sprintf("%s[%s:%s] returned %d results", m.Collection, m.Field, m.Value, m.Count)
 }
 
 func NewConnector() *MongoConnector {
@@ -77,6 +79,23 @@ func (m *MongoConnector) randomId() (string, error) {
 	return fmt.Sprintf("%x%x%x%x%x", b[0:4], b[4:6], b[6:8], b[8:10], b[10:]), nil
 }
 
+func (m *MongoConnector) ByField(collection, fieldName string, fieldValue string, result interface{}) error {
+	q := m.database.C(collection).Find(bson.M{fieldName: fieldValue})
+	count, err := q.Count()
+	if err != nil {
+		return err
+	}
+	switch count {
+	case 0:
+		return &NotFoundError{collection, fieldName, fieldValue}
+	case 1:
+		return q.One(result)
+
+	default:
+		return &ManyFoundError{collection, fieldName, fieldValue, count}
+	}
+}
+
 func (m *MongoConnector) ById(collection, id string, result interface{}) error {
 	q := m.database.C(collection).Find(m.idLike(id))
 	count, err := q.Count()
@@ -85,12 +104,12 @@ func (m *MongoConnector) ById(collection, id string, result interface{}) error {
 	}
 	switch count {
 	case 0:
-		return &NotFoundError{collection, id}
+		return &NotFoundError{collection, "id", id}
 	case 1:
 		return q.One(result)
 
 	default:
-		return &ManyFoundError{collection, id, count}
+		return &ManyFoundError{collection, "id", id, count}
 	}
 }
 
@@ -98,20 +117,7 @@ func (m *MongoConnector) ByIdOrName(collection, id string, result interface{}) e
 	err := m.ById(collection, id, result)
 	switch err.(type) {
 	case *NotFoundError:
-		q := m.database.C(collection).Find(bson.M{"name": id})
-		count, err := q.Count()
-		if err != nil {
-			return err
-		}
-		switch count {
-		case 0:
-			return &NotFoundError{collection, id}
-		case 1:
-			return q.One(result)
-
-		default:
-			return &ManyFoundError{collection, id, count}
-		}
+		return m.ByField(collection, "name", id, result)
 	default:
 		return err
 	}
