74d2709522f71ecc897635d0863b4010d7d57617
Eric Bellemon <eric.bellemon@gmail.com>
2014-12-06 23:19:10 +0100

    Split BZK_REGISTRY_HOST option

--- scripts/push-images.sh
+++ scripts/push-images.sh
@@ -5,7 +5,7 @@ set -e
 : ${BZK_REGISTRY_EMAIL:?"BZK_REGISTRY_EMAIL has to be set."}
 : ${BZK_REGISTRY_PASSWORD:?"BZK_REGISTRY_PASSWORD has to be set."}
 : ${BZK_REGISTRY_USER:?"BZK_REGISTRY_USER has to be set."}
-: ${BZK_REGISTRY_HOST:?"BZK_REGISTRY_HOST has to be set."}
+: ${BZK_REGISTRY_HOST:?"BZK_REGISTRY_HOST has to be set. e.g. registry.haklop.org"}
 
 if [ "$(uname)" != "Darwin" ]; then
   s=sudo
@@ -13,7 +13,7 @@ fi
 
 export PREFIX=$s
 
-$s docker login --email="$BZK_REGISTRY_EMAIL" --password="$BZK_REGISTRY_PASSWORD" --username="$BZK_REGISTRY_USER" $BZK_REGISTRY_HOST
+$s docker login --email="$BZK_REGISTRY_EMAIL" --password="$BZK_REGISTRY_PASSWORD" --username="$BZK_REGISTRY_USER" https://$BZK_REGISTRY_HOST
 
 docker_projects=( "parser" "parserlang/golang" "parserlang/java" "orchestration" \
 "server" "runner/golang" "runner/java" "scm/git" )
