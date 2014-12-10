9b7adee4d6f00f37d4be3968773e1879bed44665
jawher <jawher.moussa@gmail.com>
2014-12-10 02:02:56 +0100

    Reuse commons.Config in the lang-parsers config types

--- parserlang/golang/types.go
+++ parserlang/golang/types.go
@@ -5,18 +5,6 @@ import (
 )
 
 type ConfigGolang struct {
-	Language      string           `yaml:"language"`
-	Setup         []string         `yaml:"setup,omitempty"`
-	BeforeInstall []string         `yaml:"before_install,omitempty"`
-	Install       []string         `yaml:"install,omitempty"`
-	BeforeScript  []string         `yaml:"before_script,omitempty"`
-	Script        []string         `yaml:"script,omitempty"`
-	AfterScript   []string         `yaml:"after_script,omitempty"`
-	AfterSuccess  []string         `yaml:"after_success,omitempty"`
-	AfterFailure  []string         `yaml:"after_failure,omitempty"`
-	Services      []string         `yaml:"services,omitempty"`
-	Env           []string         `yaml:"env,omitempty"`
-	GoVersions    []string         `yaml:"go,omitempty"`
-	FromImage     string           `yaml:"from"`
-	Matrix        lib.ConfigMatrix `yaml:"matrix,omitempty"`
+	Base       lib.Config `yaml:",inline"`
+	GoVersions []string   `yaml:"go,omitempty"`
 }
