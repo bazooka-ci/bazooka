dd1622cb3d221f09c8129587bff093167787414b
Julien Vey <vey.julien@gmail.com>
2014-12-10 17:53:47 +0100

    remove unused methods

--- commons/docker.go
+++
@@ -1,9 +0,0 @@
-package bazooka
-
-import "strings"
-
-func SafeDockerString(s string) string {
-	safeString := strings.Replace(s, ":", "_", -1)
-	safeString = strings.Replace(safeString, "/", "_", -1)
-	return safeString
-}
