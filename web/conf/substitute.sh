d20e90cc90e535658479b7a5000e49beca74d147
jawher <jawher.moussa@gmail.com>
2014-12-11 19:20:55 +0100

    use exec to start nginx so that signals get forwarded correctly

--- web/conf/substitute.sh
+++ web/conf/substitute.sh
@@ -4,4 +4,4 @@ set -eu
 echo "injecting env vars"
 /bin/sed -i "s/<bzk_server_placeholder>/${SERVER_PORT_3000_TCP_ADDR}:${SERVER_PORT_3000_TCP_PORT}/" /etc/nginx/conf.d/default.conf
 
-nginx -g "daemon off;"
+exec nginx -g "daemon off;"
