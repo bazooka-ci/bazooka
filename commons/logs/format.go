aad311fd9d66c01acc0619e0b50e7abae6fd5b17
Julien Vey <vey.julien@gmail.com>
2014-12-30 15:08:33 +0100

    Remove time in formatter, this is handled by mongo

--- commons/logs/format.go
+++ commons/logs/format.go
@@ -4,7 +4,6 @@ import (
 	"bytes"
 	"fmt"
 	"strings"
-	"time"
 
 	log "github.com/Sirupsen/logrus"
 )
@@ -16,7 +15,7 @@ func (f *BzkFormatter) Format(entry *log.Entry) ([]byte, error) {
 
 	b := &bytes.Buffer{}
 
-	fmt.Fprintf(b, "%s [%s] %s ", entry.Time.Format(time.RFC3339), strings.ToUpper(entry.Level.String()), entry.Message)
+	fmt.Fprintf(b, "[%s] %s ", strings.ToUpper(entry.Level.String()), entry.Message)
 
 	for k, v := range entry.Data {
 		fmt.Fprintf(b, "%v=%s ", k, v)
