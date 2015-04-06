e8916a448569bd940a8ac3ed591e683e8a05e6ac
jawher <jawher.moussa@gmail.com>
2015-04-06 07:50:33 +0200

    This first commit disentangles calling the language parser container and retrieving its output from the permutations generation to make it easier to skip language parsing altogether if the language key is not set
    Fixes #49: use yaml to flush the meta file

--- parser/language_parser.go
+++ parser/language_parser.go
@@ -5,6 +5,7 @@ import (
 	"os"
 
 	log "github.com/Sirupsen/logrus"
+	lib "github.com/bazooka-ci/bazooka/commons"
 	docker "github.com/bywan/go-dockercommand"
 )
 
@@ -16,7 +17,13 @@ type LanguageParser struct {
 	Image string
 }
 
-func (p *LanguageParser) Parse() error {
+type variantData struct {
+	counter string
+	config  *lib.Config
+	meta    map[string]interface{}
+}
+
+func (p *LanguageParser) Parse() ([]*variantData, error) {
 
 	log.WithFields(log.Fields{
 		"image": p.Image,
@@ -24,7 +31,7 @@ func (p *LanguageParser) Parse() error {
 
 	client, err := docker.NewDocker(dockerEndpoint)
 	if err != nil {
-		return err
+		return nil, err
 	}
 	bazookaHome := os.Getenv("BZK_HOME")
 	container, err := client.Run(&docker.RunOptions{
@@ -37,17 +44,17 @@ func (p *LanguageParser) Parse() error {
 		Detach: true,
 	})
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	container.Logs(p.Image)
 
 	exitCode, err := container.Wait()
 	if err != nil {
-		return err
+		return nil, err
 	}
 	if exitCode != 0 {
-		return fmt.Errorf("Error during execution of Language Parser container %s/parser\n Check Docker container logs, id is %s\n", p.Image, container.ID())
+		return nil, fmt.Errorf("Error during execution of Language Parser container %s/parser\n Check Docker container logs, id is %s\n", p.Image, container.ID())
 	}
 
 	err = container.Remove(&docker.RemoveOptions{
@@ -55,8 +62,73 @@ func (p *LanguageParser) Parse() error {
 		RemoveVolumes: true,
 	})
 	if err != nil {
-		return err
+		return nil, err
 	}
 	log.Info("Language parsing finished")
-	return nil
+
+	// if all went well, the parser should have generated one or more "sub" .bazooka.*.yml files
+	// one for each compiler version for example
+	//
+	// they are also supposed to enrich it with a from attribute corresponding to a base docker image
+	// to be used to run the build
+
+	files, err := lib.ListFilesWithPrefix(OutputFolder, ".bazooka")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	res := make([]*variantData, len(files))
+
+	// for each of those files (the "sub" .bazooka.*.yml)
+	for i, file := range files {
+		// parse the damned thing
+		config := &lib.Config{}
+		err = lib.Parse(file, config)
+		if err != nil {
+			return nil, err
+		}
+
+		// extract the "*" part from the .bazooka.*.yml file
+		rootCounter := parseCounter(file)
+		// for every .bazooka.*.yml file, the language parser is also supposed to have generated a meta/* file
+		// which is a simple yml file containing the language specific  matrix variables
+		// for example, if the original .bazooka.yml file defined 2 go versions:
+		//
+		// go:
+		// - 1.2.2
+		// - 1.3.1
+		//
+		// the language parser should generate 2 meta files, one for each go version in this format:
+		//
+		// go: 1.2.2
+		//
+		// and
+		//
+		// go: 1.3.1
+		rootMetaFile := fmt.Sprintf("%s/%s", MetaFolder, rootCounter)
+		// since we have no idea of the generated meta file structure, we'll parse it into a map[string]interface{}
+		var langExtraVars map[string]interface{}
+		err := lib.Parse(rootMetaFile, &langExtraVars)
+		if err != nil {
+			return nil, err
+		}
+		res[i] = &variantData{
+			counter: rootCounter,
+			config:  config,
+			meta:    langExtraVars,
+		}
+		// after we're done iterating over the .bazooka.*.yml, and since we generated a new set of build files
+		// we can now safely remove them
+		err = os.Remove(file)
+		if err != nil {
+			return nil, fmt.Errorf("Error while removing file %s: %v", file, err)
+		}
+
+		// same for the meta files
+		err = os.Remove(rootMetaFile)
+		if err != nil {
+			return nil, fmt.Errorf("Error while removing meta folders: %v", err)
+		}
+	}
+	return res, nil
 }
