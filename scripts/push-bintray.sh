6f937d2f4ea089b55afdf7f996e47fd127e7de2b
Julien Vey <vey.julien@gmail.com>
2015-05-07 15:45:15 +0200

    Update for bintray push

--- scripts/push-bintray.sh
+++ scripts/push-bintray.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
-BINTRAY_PATH="https://api.bintray.com/content/bazooka/bazooka/bzk/nightly/"
-
 versions=( "linux_amd64" "linux_386" "linux_arm" "darwin_amd64" "darwin_386")
 
-for version in "${versions[@]}"
-do
-  # Upload to bintray
-  curl -u "$BINTRAY_USER:$BINTRAY_API_KEY" \
-    --data-binary "@bzk_$version" \
-    -H "X-Bintray-Publish: 1" -H "X-Bintray-Override: 1" \
-    -X PUT "$BINTRAY_PATH/bzk_$version/bzk"
-done
+if [ -n "$DO_PUSH" ]; then
+  if [ -z "$BZK_VERSION" ]; then
+    BZK_VERSION=latest
+  fi
+  BINTRAY_PATH="https://api.bintray.com/content/bazooka/bazooka/bzk/$BZK_VERSION/"
+  for version in "${versions[@]}"
+  do
+    # Upload to bintray
+    curl -u "$BINTRAY_USER:$BINTRAY_API_KEY" \
+      --data-binary "@bzk_$version" \
+      -H "X-Bintray-Publish: 1" -H "X-Bintray-Override: 1" \
+      -X PUT "$BINTRAY_PATH/bzk_$version/bzk"
+  done
+else
+  echo "Variable DO_PUSH not defined, skipping bintray push"
+fi
