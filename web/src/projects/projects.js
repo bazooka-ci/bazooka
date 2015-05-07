aba81d2b9005b3b52e599128974fba67ba8f7b62
jawher <jawher.moussa@gmail.com>
2015-05-07 06:42:19 +0200

    Add growl-like notification after project creation

--- web/src/projects/projects.js
+++ web/src/projects/projects.js
@@ -10,17 +10,17 @@ angular.module('bzk.projects').config(function($routeProvider) {
     });
 });
 
-angular.module('bzk.projects').controller('ProjectListController', function($scope, BzkApi, $rootScope, $routeParams) {
+angular.module('bzk.projects').controller('ProjectListController', function($scope, BzkApi, $rootScope, $routeParams, growl) {
     var pId = $routeParams.pid;
-
     BzkApi.project.list().success(function(projectList) {
         $scope.projectList = projectList;
     });
 
     $scope.createProject = function() {
         BzkApi.project.create($scope.project).success(function() {
+            growl.success('New project <strong>'+$scope.project.name+'</strong> created');
             $scope.project = {};
-            
+
             BzkApi.project.list().success(function(projectList) {
                 $scope.projectList = projectList;
                 $rootScope.$broadcast('project.new');
