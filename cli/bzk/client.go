7eb5ac7d7451ff8c5662bb95b209724b72dbf407
jawher <jawher.moussa@gmail.com>
2015-11-17 15:36:20 +0100

    Homogenize to use URL instead of URI

--- cli/bzk/client.go
+++ cli/bzk/client.go
@@ -15,27 +15,27 @@ func NewClient() (*client.Client, error) {
 	}
 
 	return client.New(&client.Config{
-		URL:      checkServerURI(*bzkApiUri, cliConfig),
+		URL:      checkServerURL(*bzkApiUrl, cliConfig),
 		Username: cliConfig.Username,
 		Password: cliConfig.Password,
 	})
 }
 
-func checkServerURI(endpoint string, cliConfig *Config) string {
+func checkServerURL(endpoint string, cliConfig *Config) string {
 	if len(endpoint) == 0 {
-		if len(cliConfig.ApiURI) == 0 {
-			var defaultURI = "http://localhost:3000"
+		if len(cliConfig.ApiURL) == 0 {
+			var defaultURL = "http://localhost:3000"
 			if runtime.GOOS == "darwin" {
-				defaultURI = "http://192.168.59.103:3000"
+				defaultURL = "http://192.168.59.103:3000"
 			}
-			endpoint = interactiveInput("Bazooka Server URI", defaultURI)
-			cliConfig.ApiURI = endpoint
+			endpoint = interactiveInput("Bazooka Server URL", defaultURL)
+			cliConfig.ApiURL = endpoint
 
 			if err := saveConfig(cliConfig); err != nil {
 				log.Fatal(fmt.Errorf("Unable to save Bazooka config, reason is: %v\n", err))
 			}
 		}
-		return cliConfig.ApiURI
+		return cliConfig.ApiURL
 	}
 	return endpoint
 }
